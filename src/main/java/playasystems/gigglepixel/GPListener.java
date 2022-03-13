2e91208847c25b5652ca56b2e50904c620bff90b
Mike Schiraldi <git@schiraldi.org>
2022-03-12 22:09:05 -0800

    Aforementioned cleanup

--- src/main/java/playasystems/gigglepixel/GPListener.java
+++ src/main/java/playasystems/gigglepixel/GPListener.java
@@ -4,26 +4,28 @@ import java.io.IOException;
 import java.net.*;
 import java.util.Arrays;
 
+import static java.net.StandardSocketOptions.SO_REUSEPORT;
 import static playasystems.gigglepixel.GPUDP.*;
 
 public class GPListener {
   private DatagramSocket socket = null;
 
   // Passes null, which means "0.0.0.0" or "Listen for packets on all available interfaces"
-  public GPListener() throws SocketException {
+  public GPListener() throws IOException {
     this(null, GP_DEFAULT_PORT);
   }
 
-  public GPListener(InetAddress listenIP) throws SocketException {
+  public GPListener(InetAddress listenIP) throws IOException {
     this(listenIP, GP_DEFAULT_PORT);
   }
 
-  public GPListener(InetAddress listenIP, int port) throws SocketException {
+  public GPListener(InetAddress listenIP, int port) throws IOException {
     // We want to listen on the provided IP+port, but we can't bind it right away.
     // We need to first pass null, then enable reuse, *then* bind.
     SocketAddress sockAddr = new InetSocketAddress(listenIP, port);
     this.socket = new DatagramSocket(null);
     this.socket.setReuseAddress(true);
+    socket.setOption(SO_REUSEPORT, true);
     this.socket.bind(sockAddr);
     this.socket.setSoTimeout(1);
   }
--- src/main/java/playasystems/gigglepixel/GPUDP.java
+++ src/main/java/playasystems/gigglepixel/GPUDP.java
@@ -1,7 +1,5 @@
 package playasystems.gigglepixel;
 
-import java.net.*;
-
 public class GPUDP {
   public static final int GP_MAXLEN = 1000;
   public static final int GP_DEFAULT_PORT = 7016;
